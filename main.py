890e2c8629cd7c2668c272add5e656dd00a8bbeb
Kyle <kkingsbe@gmail.com>
2017-10-03 19:29:38 -0400

    Sudo code

--- main.py
+++ main.py
@@ -1,22 +1,83 @@
 import httplib2,timeit
-from BeautifulSoup import BeautifulSoup, SoupStrainer
+#from BeautifulSoup import BeautifulSoup, SoupStrainer
 
-start = timeit.timeit() #Start Timer
+start_link = 'https://en.wikipedia.org/wiki/Special:AllPages/' #Set Starting Link
+while True:     #Change to for loop
+    num = 0
+    letter = 'A'
 
-#Set Starting Link
+    http = httplib2.Http()
 
-#Make an HTTP request
+    for
 
-#Find first link on wiki page
+    if num == 0:
+        letter = 'A'
+    if num == 1:
+        letter = 'B'
+    if num == 2:
+        letter = 'C'
+    if num == 3:
+        letter = 'D'
+    if num == 4:
+        letter = 'E'
+    if num == 5:
+        letter = 'F'
+    if num == 6:
+        letter = 'G'
+    if num == 7:
+        letter = 'H'
+    if num == 8:
+        letter = 'I'
+    if num == 9:
+        letter = 'J'
+    if num == 10:
+        letter = 'K'
+    if num == 11:
+        letter = 'L'
+    if num == 12:
+        letter = 'M'
+    if num == 13:
+        letter = 'N'
+    if num == 14:
+        letter = 'O'
+    if num == 15:
+        letter = 'P'
+    if num == 16:
+        letter = 'Q'
+    if num == 17:
+        letter = 'R'
+    if num == 18:
+        letter = 'S'
+    if num == 19:
+        letter = 'T'
+    if num == 20:
+        letter = 'U'
+    if num == 21:
+        letter = 'V'
+    if num == 22:
+        letter = 'W'
+    if num == 23:
+        letter = 'X'
+    if num == 24:
+        letter = 'Y'
+    if num == 25:
+        letter = 'Z'
 
-#Make an HTTP request to first link
+    start = timeit.timeit() #Start Timer
 
-#Repeat until URL for Philosophy wikipedia page is reached
+    status, response = http.request(start_link + letter) #Make an HTTP request
 
-end = timeit.timeit() #Stop Timer
+    #Find first link on wiki page
 
-#Input into database Wikipedia page name, how many steps it took to get to the philosophy wikipedia page, and how long it took
+    #Make an HTTP request to first link
 
-#Iterate to next starting link (AA - AB)
+    #Repeat until URL for Philosophy wikipedia page is reached
 
-#Go back to beginning
+    end = timeit.timeit() #Stop Timer
+
+    f = open('database','w+')
+    #Input into database Wikipedia page name, how many steps it took to get to the philosophy wikipedia page, and how long it took
+
+    #Iterate to next starting link (AA - AB)
+
+    #Go back to beginning
