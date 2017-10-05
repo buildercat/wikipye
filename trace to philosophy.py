395e9eeef58770758f4862eb353870dfed491b2a
Kyle <kkingsbe@gmail.com>
2017-10-05 19:56:53 -0400

    This doesnt work, and I dont know why

--- trace to philosophy.py
+++ trace to philosophy.py
@@ -1,13 +1,22 @@
-import httplib2,time,requests
+import httplib2,time
 from BeautifulSoup import BeautifulSoup, SoupStrainer
 
 f = open('database.txt','r+')
 links = f.readlines()
 
-for x in range(len(links)):
+page_links = []
 
+http = httplib2.Http()
+
+for x in range(len(links)):
+    links[x] = links[x][:-2]
 
 for x in range(len(links)):
     print links[x]
-    r = requests.get('https://en.wikipedia.org/wiki/A')
-    print r.status_code
+    status, response = http.request(links[10])
+
+    for link in BeautifulSoup(response, parseOnlyThese=SoupStrainer('a')):
+        if link.has_key('href'):
+            if link['href'].startswith('/wiki/'):
+                page_links.append(link['href'])
+    print page_links[10]
