893085b81255a9bead490b6db915ea2aac4bbba3
Noah <noah@ontheplanet.net>
2017-10-09 22:18:48 -0400

    Noah Updates

--- DatabaseSetUp.py
+++ DatabaseSetUp.py
@@ -1,5 +1,21 @@
+import sqlite3
 import SQLibrary as z
-z.createTable()
-z.openFile()
-z.insertData()
+
+sqlite_file = 'D:\Noah\Documents\SQlite3Databases\database.db'
+conn = sqlite3.connect(sqlite_file)
+c = conn.cursor()
+
+link = ''
+table_name = 't'
+new_field = 'Start'
+new_field2 = 'Jumps'
+fieldType = 'TEXT'
+fieldType2 = 'INTEGER'
+new_field3 = 'id'
+fieldType3 = 'INTEGER'
+x = 0
+
+z.createTable(table_name, new_field3, new_field2, new_field, fieldType3, fieldType2, fieldType, c)
+data = z.openFile()
+z.insertData(table_name, new_field3, new_field2, new_field, x, data)
 z.close()
--- SQLibrary.py
+++ SQLibrary.py
@@ -6,14 +6,15 @@ c = conn.cursor()
 data = ['']
 link = ''
 tableName = 'table'
-new_field = 'Start'
-new_field2 = 'Jumps'
-fieldType = 'TEXT'
-fieldType2 = 'INTEGER'
-new_field3 = 'id'
-fieldType3 = 'INTEGER'
+new_field = 'Counts'
+new_field2 = 'Path'
+fieldType = 'INTEGER'
+fieldType2 = 'TEXT'
+new_field3 = 'Start'
+fieldType3 = 'TEXT'
 x = 0
-f = None
+f = open('database', "r")
+
 
 def setUp():
     sqlite_file = 'D:\Noah\Documents\SQlite3Databases\database.db'
@@ -21,44 +22,43 @@ def setUp():
     c = conn.cursor()
     x = 0
 
-def createTable():
 
-    global tableName
-    global new_field3
-    global new_field2
-    global new_field
-    global fieldType3
-    global fieldType2
-    global fieldType
+def createTable(table_name, new_field3, new_field2, new_field, fieldType3, fieldType2, fieldType, c):
+    c.execute("DROP TABLE IF EXISTS t")
     c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'\
-              .format(tn=tableName, nf=new_field3, ft=fieldType3))
-    c.execute('ALTER TABLE {tn} ({nf} {ft} )'\
-              .format(tn=tableName, nf=new_field, ft=fieldType))
+              .format(tn=table_name, nf=new_field3, ft=fieldType3))
     c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-              .format(tn=tableName, cn=new_field2, ct=fieldType2))
+              .format(tn=table_name, cn=new_field, ct=fieldType))
+    c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
+              .format(tn=table_name, cn=new_field2, ct=fieldType2))
+    c.execute("ALTER TABLE t ADD COLUMN 'Loop' TEXT")
     print('createTable')
+
+
 def openFile():
-    global data
-    global link
     global f
-    f = open('database', "r")
     data = f.readlines()
     for link in range(len(data)):
         data[link] = data[link][:-2]
         print('openFile')
+    print(data)
+    return data
 
-def insertData():
-    global tableName
-    global new_field3
-    global new_field2
-    global new_field
-    global link
-    global x
+
+def insertData(tableName, new_field3, new_field2, new_field, x, data):
+    x=-1
+    pig = 'test'
+    print(data)
     for link in data:
+
+        print(link)
+        link = link.split(',')
+        v,b,n,m = link
+        b = int(b)
         x = x + 1
-        c.execute("INSERT OR IGNORE INTO {tn} ({idf}, {cn}) VALUES (x, link)".\
-            format(tn=tableName, idf=new_field3, cn=new_field))
+        c.execute("INSERT OR IGNORE INTO t VALUES (?, ?, ?, ?)", (v, b, n, m))
         print('insertData')
+
 
 def close():
     global f
