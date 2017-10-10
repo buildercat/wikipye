838b470a657c14fd2014bd328986a3b46754f21c
Kyle <kkingsbe@gmail.com>
2017-10-09 20:24:10 -0400

    Only 1 more error- ResponseNotReady

--- link tracing.py
+++ link tracing.py
@@ -17,7 +17,7 @@ print links
 f = open('database.txt','w')
 
 for i in links:
-    f = open('database.txt', 'r+')  #deletes old database filepageLinks = []
+    #f = open('database.txt', 'r+')  #deletes old database filepageLinks = []
     http = httplib2.Http()
     status, response = http.request(i)
 
@@ -28,7 +28,7 @@ for i in links:
     linkNum = len(pageLinks)
     print 'Number of links on page: ' + str(linkNum)
 
-    f.write('<' + str(i) + '> ' + '<' + str(linkNum) + '>' + ' ' + '<')
+    f.write(str(i) + ',' + str(linkNum) + ',')
     fileLen.append('another one')
     #f.write(str(linkNum) + ' \n')
     #print pageLinks
@@ -40,18 +40,16 @@ for i in links:
         while i != 'https://en.wikipedia.org/wiki/Philosophy':  # main loop for going between wiki articles/
             x = 0
             status, response = http.request(i)
-            #steps = []
             for link in BeautifulSoup(response, parseOnlyThese=SoupStrainer('a')):
                 if link.has_key('href'):
                     if link['href'] != '/wiki/Wikipedia:Protection_policy#semi' and link['href'].startswith('#') == False and link['href'].startswith('/wiki/Wikipedia:') == False and link['href'].startswith('/wiki/File:') == False: #Blacklist of links not to follow
                         wikilink = 'https://en.wikipedia.org' + link['href']
                         #print 'https://en.wikipedia.org' + link['href']
                         i = 'https://en.wikipedia.org' + link['href']
-                        print steps1
+                        print steps
                         if i in steps: #Loop for if it gets stuck in a loop
                             print "I'm stuck in a loop!"
                             f.write('True' + ' \n')
-                            f.close()
                             raise StopIteration()
                             break
                         steps.append(i)
